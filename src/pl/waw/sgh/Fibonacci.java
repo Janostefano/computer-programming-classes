a1c8bf43f9889b4e1f80f1d0496e720034468ea7
Jano <jstefaniak29@gmail.com>
2020-03-23 15:58:34 +0100

    initial commit

---
+++ src/pl/waw/sgh/Fibonacci.java
@@ -0,0 +1,31 @@
+package pl.waw.sgh;
+
+public class Fibonacci {
+
+    public static int calculateNFibonacciNumber (int number){
+        if (number == 1){
+            return 0;
+        }
+        else if (number ==2){
+            return 1;
+        }
+        else if (number == 3){
+            return 1;
+        }
+
+        return calculateNFibonacciNumber(number -1) + calculateNFibonacciNumber(number -2);
+
+
+    }
+
+    public static int calculateFibonacciSum(int items){
+
+        int sum = 0;
+
+        for (int i = 0; i < items; i++){
+            sum += calculateNFibonacciNumber(i + 1);
+        }
+
+        return sum;
+    }
+}
