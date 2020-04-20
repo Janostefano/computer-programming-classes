9ab8e82773d7b2db01400ad98ad2ac3735a01273
Jano <jstefaniak29@gmail.com>
2020-04-20 13:56:49 +0200

    add directory counter

--- src/pl/waw/sgh/DirectoryCounter.java
+++ src/pl/waw/sgh/DirectoryCounter.java
@@ -1,4 +1,36 @@
 package pl.waw.sgh;
 
+import java.io.File;
+
 public class DirectoryCounter {
+
+    public static void main(String[] args) {
+        File folder = new File("./src/pl/waw/sgh");
+        System.out.println(countFiles(folder));
+        System.out.println(countFilesSize(folder));
+    }
+
+    public static int countFiles(File directory){
+        int sum = 0;
+        if (!directory.isDirectory()){
+            System.out.println("This is not a directory");
+        } else {
+            for (File fileInDirectory: directory.listFiles()){
+                sum ++;
+            }
+        }
+        return sum;
+    }
+
+    public static long countFilesSize(File directory){
+        long sum = 0;
+        if (!directory.isDirectory()){
+            System.out.println("This is not a directory");
+        } else {
+            for (File fileInDirectory: directory.listFiles()){
+                sum += fileInDirectory.length();
+            }
+        }
+        return sum;
+    }
 }
